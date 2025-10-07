cc2dd426b48d94118105ea18008fc83b2a395e24
adharsh <Harsh.shokka@gmail.com>
2025-10-07 17:44:20 -0600

    started the lab

--- ListyCity/app/src/main/java/com/example/lab5_starter/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/lab5_starter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lab5_starter;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -11,7 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity implements CityDialogFragment.CityDialogListener {
 
@@ -20,6 +28,11 @@ public class MainActivity extends AppCompatActivity implements CityDialogFragmen
 
     private ArrayList<City> cityArrayList;
     private ArrayAdapter<City> cityArrayAdapter;
+
+    // Data base variables
+    private FirebaseFirestore db;
+    private CollectionReference citiesRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +44,30 @@ public class MainActivity extends AppCompatActivity implements CityDialogFragmen
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        // Data base variables
+        db = FirebaseFirestore.getInstance();
+        citiesRef = db.collection("cities");
+
+        // adding snapshot listener to the collections
+        citiesRef.addSnapshotListener((value, error) -> {
+
+            if(error != null){
+                Log.e("Firestore", error.toString());
+            }
+            if(value != null && !value.isEmpty()){
+                cityArrayList.clear();
+                for(QueryDocumentSnapshot snapshot: value){
+                    String name = snapshot.getString("name");
+                    String province = snapshot.getString("province");
+
+                    cityArrayList.add(new City(name,province));
+                }
+                cityArrayAdapter.notifyDataSetChanged();
+            }
+        });
+
 
         // Set views
         addCityButton = findViewById(R.id.buttonAddCity);
